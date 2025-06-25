60fdad68b3c81c9fb576eb6e198fd75820a2742b
mark-staging <143711609+markDoesany@users.noreply.github.com>
2025-06-25 22:56:31 +0800

    Update connectDB.js

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () =>{
   try{
-    const conn =  await mongoose.connect("mongodb+srv://smokey:TriuneGod143@threads.zuoivp0.mongodb.net/threads?retryWrites=true&w=majority&appName=threads", {
+    const conn =  await mongoose.connect(process.env.MONGO_URI, {
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
       // useCreateIndex: true
